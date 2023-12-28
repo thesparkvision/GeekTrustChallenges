7c8b14cb91f9be736cf17e492b70b632e06840ea
Aman Pandya <amanpandya712@gmail.com>
2023-12-28 00:26:25 +0000

    feat: Improved MetroCard Challenged code by brekaing into smaller parts and removing non real world objects

--- challenges/MetroCard-Challenge/MetroCard/src/schemas.py
+++ challenges/MetroCard-Challenge/MetroCard/src/schemas.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from datetime import date, datetime
 
 TRAVEL_CHARGES_TYPE = {
@@ -13,181 +13,100 @@ TRANSACTION_TYPES = {
     "CREDIT"
 }
 
-class CheckIn:
-    def __init__(
-        self, 
-        metro_card_id: str, 
-        passenger_type: str, 
-        from_station: str, 
-        initial_travel_charge: int,
-        total_charge_collected: int,
-        recharge_service_charge: int = 0,
-        discount_amount: int = 0,
-    ) -> None:
-        self.metro_card_id = metro_card_id
-        self.passenger_type = passenger_type
-        self.from_station = from_station
-        self.initial_travel_charge = initial_travel_charge
-        self.total_charge_collected = total_charge_collected
-        self.recharge_service_charge = recharge_service_charge
-        self.discount_amount = discount_amount
-
-class Transaction:
-    def __init__(self, amount: int, transaction_type: str):
-        self.amount = amount
-        self.flow_type = transaction_type
-
 class MetroCard:
     def __init__(self, id: str, initial_balance: int = 0):
         self.id: str = id
         self.__balance: int = initial_balance
-        self.check_in_logs: Dict[date, List[CheckIn]] = {}
-        self.transactions = []
+        self.log_entries: List[Tuple(str, int)] = []
 
-        if initial_balance > 0:
-            self.transactions.append(Transaction(initial_balance, "CREDIT"))
+    def has_discount_available(self) -> bool:
+        log_entry_count = len(self.log_entries)
+        
+        if log_entry_count > 0 and log_entry_count % 2 != 0:
+            return True
+        return False
 
-    @property
-    def current_check_in_logs_count(self) -> CheckIn:
-        current_date = datetime.now().date()
-        todays_log = self.check_in_logs.get(current_date, [])
-        return len(todays_log)
-    
-    def add_check_in_log(self, check_in: CheckIn):
-        current_date = datetime.now().date()
-        todays_log = self.check_in_logs.get(current_date)
-        if not todays_log:
-            self.check_in_logs[current_date] = [check_in]
-        else:
-            todays_log.append(check_in)
+    def find_discount_percent(self) -> int:
+        discount_percent = 0
+        if self.has_discount_available():
+            discount_percent = RETURN_JOURNEY_DISCOUNT_PERCENT
+        return discount_percent
+
+    def add_log_entry(self, from_station: str, collected_amount: int) -> None:
+        log_entry = (from_station, collected_amount)
+        self.log_entries.append(log_entry)
 
     def get_balance_amount(self) -> int:
         return self.__balance
     
     def add_balance_amount(self, amount) -> int:
         self.__balance += amount
-        self.transactions.append(Transaction(amount, "CREDIT"))
         return self.__balance
     
     def deduct_balance_amount(self, amount) -> int:
         if amount > self.__balance:
             raise Exception("Not enough amount to deduct!")
         self.__balance -= amount
-        self.transactions.append(Transaction(amount, "DEBIT"))
         return self.__balance
-        
-class MetroCardRepository:
-    def __init__(self):
-        self.metro_cards: Dict[str, MetroCard] = {}
-        
-    def create_metro_card(self, id: str, initial_balance: int = 0) -> None:
-        if not id:
-            raise Exception("Metro Card need to have an ID!")
-        
-        if id in self.metro_cards:
-            raise Exception("Metro Card need to have unique ID. This ID is in assigned to somebody else!")
-        
-        metro_card = MetroCard(id, initial_balance)
-        self.metro_cards[metro_card.id] = metro_card  
 
-    def get_metro_card(self, id: str) -> MetroCard:
-        return self.metro_cards.get(id)
-
-class StationDailyRecordBook:
-    def __init__(self):
+class Station:
+    def __init__(self, name: str):
+        self.name = name
         self.collected_amount: int = 0
-        self.given_discount: int = 0
-        self.passenger_counts: Dict[str, int] = {}
+        self.discount_given: int = 0
+        self.passenger_count_details: Dict[str, int] = {}
 
-    def add_entry(self, amount: int, passenger_type: str, discount_amount: int = 0):
-        passenger_count = self.passenger_counts.get(passenger_type, 0)
-        self.passenger_counts[passenger_type] = passenger_count + 1
-        self.given_discount += discount_amount
+    def get_station_name(self):
+        return self.name
+
+    def update_station_collection(self, amount: int, passenger_type: str, discount_amount: int = 0):
+        passenger_count = self.passenger_count_details.get(passenger_type, 0)
+        self.passenger_count_details[passenger_type] = passenger_count + 1
+        self.discount_given += discount_amount
         self.collected_amount += amount
     
-class Station:
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.daily_records: Dict[date, StationDailyRecordBook] = {}
-    
-    def update_daily_record(self, amount, passenger_type, discount_amount):
-        current_date = datetime.now().date()
-        todays_record = self.daily_records.get(current_date) 
-        if not todays_record:
-            todays_record = StationDailyRecordBook()
-            todays_record.add_entry(amount, passenger_type, discount_amount)
-            self.daily_records[current_date] = todays_record 
-        else:
-            todays_record.add_entry(amount, passenger_type, discount_amount)
-         
-    def get_record_entry(self, entry_date: date) -> StationDailyRecordBook:
-        record_entry = self.daily_records.get(entry_date)
-        if not record_entry:
-            raise Exception(f"Record Entry in Station {self.name} for {entry_date} not found!")
-        return record_entry
-    
-    def print_record_entry(self, entry_date: date) -> None:
-        record_entry = self.get_record_entry(entry_date)
-        print("TOTAL_COLLECTION", self.name, record_entry.collected_amount, record_entry.given_discount)
+    def display_station_collection_details(self):
+        print("TOTAL_COLLECTION", self.name, self.collected_amount, self.discount_given)
         print("PASSENGER_TYPE_SUMMARY")
-        for passenger_type, passenger_count in record_entry.passenger_counts.items():
+        
+        passenger_count_details = list(self.passenger_count_details.items())
+        passenger_count_details.sort(key = lambda x: (-x[1], x[0]))
+        for passenger_type, passenger_count in passenger_count_details:
             print(passenger_type, passenger_count)
 
-class StationRepository:
-    def __init__(self):
-        self.stations: Dict[str, Station] = {}
+class StationCheckIn:
+    def __init__(
+        self,
+        station: Station,
+        metro_card: MetroCard,
+        passenger_type: str
+    ):
+        self.station = station
+        self.metro_card = metro_card
+        self.passenger_type = passenger_type
+        self.total_amount_charged = TRAVEL_CHARGES_TYPE[passenger_type]
+        self.discount_amount = 0
 
-    def create_station(self, name: str) -> Station:
-        if name in self.stations:
-            raise Exception(f"Station with name {name} already present!")
-        
-        new_station = Station(name)
-        self.stations[name] = new_station
-    
-    def get_station(self, name: str) -> Station:
-        station = self.stations.get(name)
-        if not station:
-            raise Exception(f"Station {name} not found")
-        return station
-    
-    def user_check_in(self, metro_card: MetroCard, passenger_type: str, from_station: str):
-        user_station = self.get_station(from_station)
+    def apply_discount_if_any(self):
+        discount_percent = self.metro_card.find_discount_percent()
+        self.discount_amount = int(self.total_amount_charged * discount_percent)
+        self.total_amount_charged = self.total_amount_charged - self.discount_amount
 
-        travel_charge = TRAVEL_CHARGES_TYPE[passenger_type]
-        total_amount = travel_charge
+    def pay_final_amount(self):
         recharge_service_charge = 0
-
-        discount_amount = 0
-        check_in_count = metro_card.current_check_in_logs_count
-        if check_in_count > 0 and check_in_count % 2 != 0:
-            discount_amount = int(total_amount * RETURN_JOURNEY_DISCOUNT_PERCENT)
-        total_amount = total_amount - discount_amount
-
-        extra_balance_required = 0
-        balance_amount = metro_card.get_balance_amount()
-        if balance_amount - total_amount < 0:
-            extra_balance_required = total_amount - balance_amount
+        balance_amount = self.metro_card.get_balance_amount()
+        if balance_amount - self.total_amount_charged < 0:
+            extra_balance_required = self.total_amount_charged - balance_amount
             recharge_service_charge = int(extra_balance_required * RECHARGE_SERVICE_TAX_PERCENT)
-            metro_card.add_balance_amount(extra_balance_required)
-            metro_card.deduct_balance_amount(total_amount)
-        else:
-            metro_card.deduct_balance_amount(total_amount)
-        
-        total_amount_collected = total_amount + recharge_service_charge
+            self.metro_card.add_balance_amount(extra_balance_required)
+            
+        self.metro_card.deduct_balance_amount(self.total_amount_charged)
+        self.total_amount_charged = self.total_amount_charged + recharge_service_charge
 
-        user_station.update_daily_record(
-            amount=total_amount_collected,
-            passenger_type=passenger_type,
-            discount_amount=discount_amount
+        self.station.update_station_collection(
+            amount=self.total_amount_charged,
+            passenger_type=self.passenger_type,
+            discount_amount=self.discount_amount
         )
-
-        check_in = CheckIn(
-            metro_card_id = metro_card.id, 
-            passenger_type = passenger_type, 
-            from_station = from_station, 
-            initial_travel_charge = travel_charge,
-            total_charge_collected = total_amount_collected,
-            recharge_service_charge = recharge_service_charge,
-            discount_amount = discount_amount,
-        )
-        metro_card.add_check_in_log(check_in)
+        station_name = self.station.get_station_name()
+        self.metro_card.add_log_entry(station_name, self.total_amount_charged)
--- challenges/MetroCard-Challenge/MetroCard/src/solution.py
+++ challenges/MetroCard-Challenge/MetroCard/src/solution.py
@@ -1,46 +1,56 @@
 from typing import List
 from datetime import date, datetime
 
-from .schemas import MetroCardRepository, StationRepository
+from .schemas import MetroCard, Station, StationCheckIn
+
+AIRPORT_STATION_NAME = "AIRPORT"
+CENTRAL_STATION_NAME = "CENTRAL"
 
 class Solution(object):
 
     def __init__(self, input_lines: List[str]) -> None:
         self.input_lines = input_lines
-        self.metro_card_repository = MetroCardRepository()
         
-        station_repository = StationRepository()
-        station_repository.create_station(name="AIRPORT")
-        station_repository.create_station(name="CENTRAL")
-        self.station_repository = station_repository
+        self.metro_cards_lookup = {}
 
-        self.user_check_ins = []
+        self.stations_lookup = {}
+        self.stations_lookup[AIRPORT_STATION_NAME] = Station(name=AIRPORT_STATION_NAME)
+        self.stations_lookup[CENTRAL_STATION_NAME] = Station(name=CENTRAL_STATION_NAME)
+
+        self.station_check_ins = []
 
     def process_input(self) -> None:        
         for line in self.input_lines:
             if line.startswith("BALANCE"):
                 _, metro_card_id, initial_balance = line.split(" ")
-                self.metro_card_repository.create_metro_card(metro_card_id, int(initial_balance))
+                metro_card = MetroCard(
+                    id=metro_card_id, 
+                    initial_balance=int(initial_balance)
+                )
+                self.metro_cards_lookup[metro_card_id] = metro_card
+
             elif line.startswith("CHECK_IN"):
                 _, metro_card_id, passenger_type, from_station = line.strip().split(" ")
-                self.user_check_ins.append((metro_card_id, passenger_type, from_station))
+                self.station_check_ins.append((metro_card_id, passenger_type, from_station))
         
     def process_output(self) -> None:
-        for check_in in self.user_check_ins:
+        for check_in in self.station_check_ins:
             metro_card_id, passenger_type, from_station = check_in
-            metro_card = self.metro_card_repository.get_metro_card(metro_card_id)
             
-            self.station_repository.user_check_in(
+            metro_card = self.metro_cards_lookup[metro_card_id]
+            station = self.stations_lookup[from_station]
+            
+            station_check_in = StationCheckIn(
+                station=station,
                 metro_card=metro_card,
-                passenger_type=passenger_type,
-                from_station=from_station
+                passenger_type=passenger_type
             )
+            station_check_in.apply_discount_if_any()
+            station_check_in.pay_final_amount()
 
     def print_output(self) -> None:
-        current_date = datetime.now().date()
-        
-        airport_station = self.station_repository.get_station("AIRPORT")
-        central_station = self.station_repository.get_station("CENTRAL")
+        airport_station = self.stations_lookup[AIRPORT_STATION_NAME]
+        central_station = self.stations_lookup[CENTRAL_STATION_NAME]
 
-        central_station.print_record_entry(current_date)
-        airport_station.print_record_entry(current_date)
+        central_station.display_station_collection_details()
+        airport_station.display_station_collection_details()
